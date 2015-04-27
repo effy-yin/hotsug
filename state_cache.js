06367c08a19152128686c9302582be39d7d63299
dodo糯 <effy.yin@gmail.com>
2015-04-27 10:55:15 +0800

    add state-cache.js

---
+++ state_cache.js
@@ -0,0 +1,77 @@
+(function(window, $) {
+	function HotSug (ele, options) {
+        var defaults = {
+            cookieName: 'words-list',            
+            hoverElem: 'div',
+            highlightElem: 'a',
+            changeTime: '2000',
+            expires: 7
+        }
+        this.$ele = $(ele);
+        this.options = $.extend(defaults, options);
+        this.init();
+    }
+
+    HotSug.prototype = {
+        init: function() {
+            var _this = this;
+            var value = $.cookie(this.options.cookieName);
+
+            if(value) {
+                var arr = value.split(',');
+                this.$ele.find(_this.options.hoverElem).each(function() {
+                    for(var i=0;i<arr.length;i++) {
+                        if($(this).data('num')==arr[i]) {
+                            $(this).find(_this.options.highlightElem).addClass('viewed');
+                        }
+                    }
+                });
+            }
+            this.bindListener();
+        },  
+        bindListener: function() {
+            var _this = this;
+            this.$ele.find(this.options.hoverElem).click(function () {
+                $(this).find(_this.options.highlightElem).addClass('viewed');
+            });
+
+            this.$ele.find(this.options.hoverElem).each(function() {
+                var timerO, timerI, longEnough = false, hoverElem = this;
+
+                $(this).mouseenter(function() {
+                    var highlightElem = $(this).find(_this.options.highlightElem);
+                    if(!highlightElem.hasClass('viewed')) {   //放在事件内判断，不能放在事件外判断，否则定时器定义语句每次鼠标enter都会执行
+             
+                        timerO = setTimeout(function() {
+                            longEnough = true;
+                        }, _this.options.changeTime);
+                        timerI = setInterval(function () {
+                            if(longEnough) {
+                                highlightElem.addClass('viewed');
+                                var index = $(hoverElem).data('num');
+                                var value = $.cookie(_this.options.cookieName) || '';
+                                if(value) {
+                                    var arr = value.split(',');
+                                    arr.push(index);
+                                    value = arr.join(',')
+                                } else {
+                                    value = index;
+                                }
+                                $.cookie(_this.options.cookieName, value, { expires: _this.options.expires});
+                            }
+                        }, 200);
+                    }            
+                }).mouseleave(function() {
+                    clearTimeout(timerO)
+                    clearInterval(timerI);                        
+                });
+            });
+
+        }
+    }
+
+    $.fn.hotSug = function(options) {
+        new HotSug(this, options);
+        return $(this);
+    }
+})(window, jQuery);
